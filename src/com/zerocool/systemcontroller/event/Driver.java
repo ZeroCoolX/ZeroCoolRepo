232ae0da6a19029a47347369caa9cf07ad89a82c
jleveque <jleveque@uwm.edu>
2015-02-21 01:47:50 -0600

    Updated Driver, changed dates into longs.

--- src/com/zerocool/systemcontroller/event/Driver.java
+++ src/com/zerocool/systemcontroller/event/Driver.java
@@ -1,5 +1,7 @@
 package com.zerocool.systemcontroller.event;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -10,16 +12,21 @@ import com.zerocool.systemcontroller.participant.Participant;
 public class Driver {
 	
 	@SuppressWarnings("deprecation")
-	public static void main (String [] args){
+	public static void main (String [] args) throws ParseException{
 		
 		System.out.println("Testing...testing....");
 		AbstractEvent[] events = new AbstractEvent[8];
 		//Calendar cal = Calendar.getInstance();
 		System.out.println("Testing each constructor: [String], [String, EventType], [String, EventType, Date], [String, EventType, Date, long]");
+
+    	SimpleDateFormat f = new SimpleDateFormat("yyyy/mm/dd hh:mm:ss.lll");
+    	Date d;
 		events[0] = new Individual("100M Sprint");
 		events[1] = new Group("CrossCountry Skiiing", AbstractEvent.EventType.GRP);
-		events[2] = new ParIndividual("Marathon", AbstractEvent.EventType.PARIND, new Date(2015, 2, 17, 9, 23, 50));
-		events[3] = new ParGroup("Swimming", AbstractEvent.EventType.PARGRP, new Date(2015, 2, 17, 9, 25, 00), 9000);	
+		d = f.parse("2015/02/17 09:23:50.000");
+		events[2] = new ParIndividual("Marathon", AbstractEvent.EventType.PARIND, d.getTime());
+		d = f.parse("2015/02/17 09:25:00.000");
+		events[3] = new ParGroup("Swimming", AbstractEvent.EventType.PARGRP, d.getTime(), 9000);	
 		System.out.println("Testing empty constructor for each class. Should be type specific");
 		events[4] = new Individual();
 		events[5] = new Group();
