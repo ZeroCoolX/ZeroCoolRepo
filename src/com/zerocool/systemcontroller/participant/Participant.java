7b29a23c0236f32f607c07ccd429e1049280ac2c
adampermann <adampermann@uwm.edu>
2015-02-20 21:37:43 -0600

    Implemented the Participant and Record classes.  Did NOT implement the formated data method for Participant and will probably change how create record method works.

--- src/com/zerocool/systemcontroller/participant/Participant.java
+++ src/com/zerocool/systemcontroller/participant/Participant.java
@@ -1,73 +1,92 @@
 package com.zerocool.systemcontroller.participant;
 
+import java.util.*;
+/**
+ * @author ZeroCool
+ * The Participant Class
+ * 
+ * This class is the representation of a 
+ * {@link #Participant} in any timed Event
+ */
 public class Participant {
-
-	public Participant() {
-		
-	}
-
-	public Participant(long id) {
-		
-	}
+	private long id;
+	private String name;
+	private Stack<Record> records;
+	private boolean isCompeting;
 	
+	/**
+	 * Creates a new instance of the Participant class
+	 * @param id
+	 * @param name
+	 */
 	public Participant(long id, String name) {
-		this(id);
-		
+		this.id = id;
+		this.name = name;
+		this.records = new Stack<Record>();
 	}
 	
 	
-	// ----- functional methods ----- \\
-	
+	// ----- Functional ----- \\
+	/**
+	 * Creates a new {@link #Record} for this {@link #Participant}.
+	 */
 	public void createNewRecord() {
-		
+		records.push(new Record());
 	}
 	
+	// ----- Access ----- \\
+	/**
+	 * Returns the last {@link #Record} created for this {@link #Participant}.
+	 * If no {@link #Record} exists then it returns null
+	 * @return The last created {@link #Record}
+	 */
 	public Record getLastRecord() {
-		
-		return null;
+		return this.records.isEmpty() ? null : this.records.peek();
 	}
 	
 	public String getFormattedData(long eventID) {
-		
 		return null;
 	}
-	
-	
-	// ----- accessors ----- \\
-	
+
+	/**
+	 * @return
+	 */
 	public long getID() {
-		
-		return -1;
+		return this.id;
 	}
 	
+	/**
+	 * Returns the number of Records that this {@link #Participant} has.
+	 * @return int
+	 */
 	public int getRecordCount() {
-		
-		return -1;
+		return this.records.size();
 	}
 	
+	/**
+	 * Returns the {@link #Participant}'s name.
+	 * @return string
+	 */
 	public String getName() {
-		
-		return null;
+		return this.name;
 	}
 	
+	/**
+	 * Get if this {@link #Participant} is competing
+	 * @return
+	 */
 	public boolean getIsCompeting() {
-		
-		return false;
+		return this.isCompeting;
 	}
 	
 	
-	// ----- mutators ----- \\
-	
-	public void setName(String name) {
-		
-	}
-	
-	public void setID(long id) {
-		
-	}
-	
+	// ----- Mutate ----- \\\	
+	/**
+	 * Sets if this {@link #Participant} is competing in an {@link #Event}
+	 * @param isCompeting
+	 */
 	public void setIsCompeting(boolean isCompeting) {
-		
+		this.isCompeting = isCompeting;
 	}
 	
 }
--- src/com/zerocool/systemcontroller/participant/Record.java
+++ src/com/zerocool/systemcontroller/participant/Record.java
@@ -1,60 +1,117 @@
 package com.zerocool.systemcontroller.participant;
 
+/**
+ * @author ZeroCool
+ * The Record Class.
+ * 
+ * This class creates a record of a participant's
+ * performance in one event.
+ */
 public class Record {
 
-	public Record() {
+	private long eventId;
+	private String eventName;
+	private long startTime;
+	private long finishTime;
+	private boolean dnf;
+	
+	/**
+	 * Creates a new instance of the Record class with values
+	 * @param startTime The participant's start time
+	 * @param finishTime The participant's finish time
+	 * @param dnf If the participant did not finish
+	 * @param eventId The event's id number
+	 * @param eventName The name of the event
+	 */
+	public Record(long startTime, long finishTime, boolean dnf, long eventId, String eventName) {
+		this.startTime = startTime;
+		this.finishTime = finishTime;
+		this.dnf = dnf;
+		this.eventId = eventId;
+		this.eventName = eventName;
+	}
+	
+	/**
+	 * Creates a new instance of the Record class without values
+	 */
+	public Record()
+	{
 		
 	}
 
-	
-	// ----- accessors ----- \\
-	
+	// ----- Access ----- \\
+	/**
+	 * @return If the participant finished the event
+	 */
 	public boolean getDnf() {
-		
-		return false;
+		return this.dnf;
 	}
 	
+	/**
+	 * @return The finish time
+	 */
 	public long getFinishTime() {
-		
-		return -1;
+		return this.finishTime;
 	}
 	
+	/**
+	 * @return The start time
+	 */
 	public long getStartTime() {
-		
-		return -1;
+		return this.startTime;
 	}
 	
+	/**
+	 * @return The event name this record is for
+	 */
 	public String getEventName() {
-		
-		return null;
+		return this.eventName;
 	}
 	
+	/**
+	 * @return The event Id
+	 */
 	public long getEventID() {
-		
-		return -1;
+		return this.eventId;
 	}
-	
-	
-	// ----- mutators ----- \\
-	
+
+	// ----- Mutate ----- \\
+	/**
+	 * Sets the event's name
+	 * @param eventName
+	 */
 	public void setEventName(String eventName) {
-		
+		this.eventName = eventName;
 	}
 	
+	/**
+	 * Sets the start time of the participant's record
+	 * @param startTime
+	 */
 	public void setStartTime(long startTime) {
-		
+		this.startTime = startTime;
 	}
 	
+	/**
+	 * Sets the finish time of the participant's record
+	 * @param finishTime
+	 */
 	public void setFinishTime(long finishTime) {
-		
+		this.finishTime = finishTime;
 	}
 	
+	/**
+	 * Sets whether the participant finished the event or not
+	 * @param dnf
+	 */
 	public void setDnf(boolean dnf) {
-		
+		this.dnf = dnf;
 	}
 	
+	/**
+	 * @param eventId
+	 */
 	public void setEventId(long eventId){
-		
+		this.eventId = eventId;
 	}
-	
 }
