1399ccb7ff182bb273bf2782726dd64171c90a1f
adampermann <adampermann@uwm.edu>
2015-02-21 19:57:12 -0600

    Added global test file so the chrono timer can be tested as a whole.

---
+++ src/com/zerocool/tests/ChronoTimerSystemTests.java
@@ -0,0 +1,13 @@
+package com.zerocool.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ChronoTimerSystemTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
