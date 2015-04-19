4f9458b2879000119abc3d6917565f101562ecbe
novaradix <novaradix@192.168.0.2>
2015-04-19 16:05:57 -0500

    Made methods in WebServiceLink to facilitate sending params to web service

--- src/com/zerocool/controllers/WebServiceLink.java
+++ src/com/zerocool/controllers/WebServiceLink.java
@@ -1,5 +1,37 @@
 package com.zerocool.controllers;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class WebServiceLink {
 
+	public static final String SERVICE_URL = "http://localhost:8888/zerocoolwebservice";
+	
+	private static void sendParams(String params){
+		try {
+			URL site = new URL(SERVICE_URL);
+			HttpURLConnection conn = (HttpURLConnection) site.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+			String content = params;
+			System.out.println(content);
+			out.writeBytes(content);
+			out.flush();
+			out.close();
+			System.out.println("Sent to server");
+			new InputStreamReader(conn.getInputStream());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private static void setTitle(String title){
+		sendParams("title="+title);
+	}
+	
 }
