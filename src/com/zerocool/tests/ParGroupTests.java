630d08941bc74f73e90e90dd088f00962ac88bec
adampermann <adampermann@uwm.edu>
2015-04-19 13:21:25 -0500

    Added JUnits for ParGroup and ParInd.  Now implementing

---
+++ src/com/zerocool/tests/ParGroupTests.java
@@ -0,0 +1,39 @@
+package com.zerocool.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParGroupTests {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testTriggered() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDnf() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParGroup() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/com/zerocool/tests/ParIndividualTests.java
@@ -0,0 +1,39 @@
+package com.zerocool.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParIndividualTests {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testTriggered() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDnf() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParIndividual() {
+		fail("Not yet implemented");
+	}
+
+}
