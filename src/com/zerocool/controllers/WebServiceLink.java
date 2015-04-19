8b0cc6264ee618c83f4ca07f99a7d0a16f77cb8c
novaradix <novaradix@192.168.0.2>
2015-04-19 16:23:22 -0500

    Making sure URLs are correct

--- src/com/zerocool/controllers/WebServiceLink.java
+++ src/com/zerocool/controllers/WebServiceLink.java
@@ -7,7 +7,7 @@ import java.net.URL;
 
 public class WebServiceLink {
 
-	public static final String SERVICE_URL = "http://localhost:8888/zerocoolwebservice";
+	public static final String SERVICE_URL = "http://localhost:8888/zc_webservice";
 	
 	private static void sendParams(String params){
 		try {
@@ -32,6 +32,30 @@ public class WebServiceLink {
 	
 	private static void setTitle(String title){
 		sendParams("title="+title);
+	}
+	
+	private static void setParticipantBib(int part_id, String bib){
+		sendParams("part_id="+part_id+"&bib="+bib);
+	}
+	
+	private static void setParticipantName(int part_id, String name){
+		sendParams("part_id="+part_id+"&name="+name);
+	}
+	
+	private static void setParticipantStart(int part_id, String start){
+		sendParams("part_id="+part_id+"&start="+start);
+	}
+	
+	private static void setParticipantEnd(int part_id, String end){
+		sendParams("part_id="+part_id+"&end="+end);
+	}
+	
+	private static void setParticipantElapsed(int part_id, String elapsed){
+		sendParams("part_id="+part_id+"&elapsed="+elapsed);
+	}
+	
+	private static void clear(){
+		sendParams("version=-1");
 	}
 	
 }
